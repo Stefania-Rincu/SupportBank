4fc3c2e61a706255128a0b308ec7c98af5827345
Stefania Rincu <Stefania.Rincu@softwire.com>
2026-07-20 14:17:21 +0300

    Solved comments

--- main.py
+++ main.py
@@ -30,6 +30,17 @@ def get_or_create_account(name, accounts):
         accounts[name.lower()] = Account(name)
     return accounts[name.lower()]
 
+def read_csv(file_name):
+    content = pd.read_csv(file_name)
+    content['Date'] = content['Date'].apply(lambda date: datetime.strptime(date, '%d/%m/%Y'))
+    return content
+
+def read_json(file_name):
+    content = pd.read_json(file_name)
+    content['Date'] = content['Date'].apply(lambda date: date.date())
+    content = content.rename(columns={'ToAccount': 'To', 'FromAccount': 'From'})
+    return content
+
 def read_xml(file_name):
     with open(file_name, encoding='utf8') as f:
         xml = f.read()
@@ -38,16 +49,9 @@ def read_xml(file_name):
 
 def read_file(file_name, extension):
     return {
-        'csv': lambda: pd.read_csv(file_name),
-        'json': lambda: pd.read_json(file_name),
+        'csv': lambda: read_csv(file_name),
+        'json': lambda: read_json(file_name),
         'xml': lambda: read_xml(file_name)
-    }[extension]()
-
-def format_date(date, extension):
-    return {
-        'csv': lambda: datetime.strptime(date, '%d/%m/%Y'),
-        'json': lambda: date.date(),
-        'xml': lambda: pd.to_datetime(int(date), unit='D', origin='1900-01-01').date()
     }[extension]()
 
 def format_transaction_details(date, to_account, from_account, narrative, amount):
@@ -63,7 +67,6 @@ def process_transaction(row_index, accounts, details):
     try:
         amount = float(details['amount'])
         date = details['date']
-        #date = format_date(details['date'], extension)
 
         for name, sign in [(details['to'], 1), (details['from'], -1)]:
             account = get_or_create_account(name, accounts)
@@ -72,32 +75,26 @@ def process_transaction(row_index, accounts, details):
 
     except Exception as exception:
         logging.error(
-            f'Error on line: {row_index}. {exception}')
+            f'Error on line: {row_index + 1}. {exception}')
 
-def parse_csv_and_json(extension, content, accounts):
-    columns_by_extension = {'csv':['To', 'From'], 'json': ['ToAccount', 'FromAccount']}
-
+def parse_csv_and_json(content, accounts):
     for row_index, transaction_details in content.iterrows():
-        date = format_date(transaction_details['Date'], extension)
-        details = format_transaction_details(date, transaction_details[columns_by_extension[extension][0]],
-                                             transaction_details[columns_by_extension[extension][1]], transaction_details['Narrative'], transaction_details['Amount'])
+        details = format_transaction_details(transaction_details['Date'], transaction_details['To'], transaction_details['From'],
+                                             transaction_details['Narrative'], transaction_details['Amount'])
 
-        if extension == 'csv':
-            process_transaction(row_index + 2, accounts, details)
         process_transaction(row_index, accounts, details)
 
     return accounts
 
-def parse_xml(extension, content, accounts):
+def parse_xml(content, accounts):
     all_transactions = content.find_all('SupportTransaction')
 
     for row_index, transaction in enumerate(all_transactions):
         try:
             date = transaction['Date']
-            date = format_date(date, extension)
+            date = pd.to_datetime(int(date), unit='D', origin='1900-01-01').date()
 
             parties = transaction.find('Parties')
-
             if parties is not None:
                 to_account = parties.find('To').text
                 from_account = parties.find('From').text
@@ -120,7 +117,6 @@ def parse_xml(extension, content, accounts):
             process_transaction(row_index, accounts, details)
 
         except Exception as exception:
-            print(exception)
             logging.error(f'Error on line: {row_index + 1}. {exception}')
 
     return accounts
@@ -129,9 +125,9 @@ def process_file_content(extension, content):
     accounts = {}
 
     return {
-        'csv': lambda: parse_csv_and_json(extension, content, accounts),
-        'json': lambda: parse_csv_and_json(extension, content, accounts),
-        'xml': lambda: parse_xml(extension, content, accounts)
+        'csv': lambda: parse_csv_and_json(content, accounts),
+        'json': lambda: parse_csv_and_json(content, accounts),
+        'xml': lambda: parse_xml(content, accounts)
     }[extension]()
 
 def load_accounts(file_name):
